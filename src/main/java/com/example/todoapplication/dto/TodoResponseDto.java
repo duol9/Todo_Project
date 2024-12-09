3a55e76939b1190b11d045364eb8c9e50c14f67a
duol9 <lhyy0195@gmail.com>
2024-12-09 15:52:11 +0900

    feat: Json 응답 시 pw 속성 제외
    @JsonIgnore 애노테이션을 사용해 Json응답에서 pw정보가 포함되지 않도록 수정

--- src/main/java/com/example/todoapplication/dto/TodoResponseDto.java
+++ src/main/java/com/example/todoapplication/dto/TodoResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.todoapplication.dto;
 
 import com.example.todoapplication.entity.Todo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,10 +9,12 @@ import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+// 응답 시 비번 제외하고 반환
 public class TodoResponseDto {
 
     private Long id;
     private String name;
+    @JsonIgnore
     private String pw;
     private String task;
     private LocalDateTime createDate;
